17f4ce07f03722bd04d5e21a21865c6563956bc7
lidthuas <niklas.bartz@gmail.com>
2024-03-08 01:44:18 +0100

    learn rust: enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+enum Action {
+    Add,
+    Delete,
+    Edit,
+}
+
+fn inspect_action( action: Action) {
+    match action {
+        Action::Add => println!("add actiion triggered"),
+        Action::Edit => println!("edit action triggered"),
+        Action::Delete => println!("delete action triggered"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut current_action: Action;
+
+
+    let action_a = Action::Add;
+    let action_d = Action::Delete;
+    let action_e = Action::Edit;
+
+    current_action = action_a;
+    inspect_action(current_action);
+    current_action = action_d;
+    inspect_action(current_action);
+    current_action = action_e;
+    inspect_action(current_action);
 }
