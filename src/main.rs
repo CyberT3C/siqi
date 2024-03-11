eb9754a9604c37a04db9998321b9cfc1d8940833
lidthuas <niklas.bartz@gmail.com>
2024-03-12 00:09:36 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 /* https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
  * https://github.com/dtolnay/serde-yaml
  *
  *
- * TODO today
- * [X] write yaml to file
- * [X] read yaml from file
- * [X] transfer data from read buffer into a my data format
+ * TODO 
+ * [X] refactore read write into file type / class / interfaces whatever?
+ * [ ] Change Yaml format, Index Information is already stored by order and not needed!
  *
  * TODO next week
  * [ ] build full POC with
  *      - add task
  *      - delete task
  *      - list tasks
+ *
+ * TODO week after
+ * [ ] build with nix 
+ * [ ] add cli support 
  *      - move up
  *      - move down
  */
@@ -27,7 +33,7 @@ struct TaskItem {
 }
 
 struct SortedTaskList {
-    data: BTreeMap<usize, TaskItem>, 
+    data: BTreeMap<usize, TaskItem>,
 }
 
 impl SortedTaskList {
@@ -72,21 +78,65 @@ impl SortedTaskList {
         }
     }
 
-    fn write_file(&self) -> std::io::Result<()> {
-        let filename = String::from("testoutput/task.yaml");
-        // default is task.yaml; maybe something like default.task later wich will
-        // get read automatically on startup?
-        let result = std::fs::write(filename, self.to_yaml());
-        result
+}
+
+struct TaskFileIO {
+    path: String,
+    filename: String,
+}
+
+impl TaskFileIO {
+    fn new() -> Self {
+        TaskFileIO {
+            path: String::from("./"), // change this for debug only later but i dont know
+            filename: String::from("default.task"),
+        }
     }
 
-    fn read_file() -> Self {
-        let input = std::fs::read_to_string("testoutput/task.yaml");
-        let yaml = match input {
-            Ok(yaml) => yaml,
-            Err(_error) => panic!("empty"),
+    fn write_file(&self, yaml: String) -> () {
+        // we only operate in the current directory
+        // let mut path = std::path::Path::new(&self.path);
+        // let display = path.display();
+        // if !path.exists() {
+        //     let res = std::fs::create_dir(path);
+        //     match res {
+        //         Ok(x) => (),
+        //         Err(e) => panic!("cannot create directory!"),
+        //     }
+        // }
+
+        let full_path = format!("{}{}", self.path, self.filename);
+        let path = std::path::Path::new(&full_path);
+        let display = path.display();
+        // change write mode later on
+        // I dont wanna desotroy the content in file or do I?
+        // Open a file in write-only mode, returns `io::Result<File>`
+        let mut file = match File::create(&full_path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why),
+            Ok(file) => file,
         };
-        SortedTaskList::from_yaml(&yaml)
+
+        match file.write_all(yaml.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why),
+            Ok(_) => println!("successfully wrote to {}", display),
+        }
+    }
+
+    fn read_file(&self) -> String {
+        let full_path = format!("{}{}", self.path, self.filename);
+        let path = std::path::Path::new(&full_path);
+        let display = path.display();
+        // Open the path in read-only mode, returns `io::Result<File>`
+        let mut file = match File::open(path) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+            Ok(file) => file,
+        };
+        let mut yaml_as_string = String::new();
+        match file.read_to_string(&mut yaml_as_string) {
+            Err(why) => panic!("couldn't read {}: {}", display, why),
+            Ok(_) => print!("{} read\n", display),
+        }
+        yaml_as_string
     }
 }
 
@@ -97,11 +147,12 @@ fn main() {
     }
     task_list.remove_by_index(0);
     task_list.remove_by_index(1);
-    task_list.print();
-
-    let _ = task_list.write_file();
+    //task_list.print();
 
     println!("----------------------");
-    let new_list = SortedTaskList::read_file();
-    new_list.print();
+
+    let task_file_io = TaskFileIO::new();
+    task_file_io.write_file(task_list.to_yaml());
+    let task_list_from_file = SortedTaskList::from_yaml(&task_file_io.read_file());
+    task_list_from_file.print();
 }
